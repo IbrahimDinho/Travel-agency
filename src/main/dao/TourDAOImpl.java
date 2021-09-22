5a1ea472b4df9f98da0ca6e90d33d7114d56e712
ibrahim <ibrahim@ibrahims-MacBook-Pro-2.local>
2021-09-22 22:15:03 +0100

    one to one relation added

--- src/main/dao/TourDAOImpl.java
+++ src/main/dao/TourDAOImpl.java
@@ -2,7 +2,6 @@ package main.dao;
 
 import java.util.List;
 
-import javax.transaction.Transactional;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
@@ -18,21 +17,18 @@ public class TourDAOImpl implements TourDAO{
 	private SessionFactory sessionFactory;
 	
 	@Override
-	@Transactional
 	public List<Tour> getAll() {
 		Session session = sessionFactory.getCurrentSession();
 		return session.createQuery("from Tour t", Tour.class).list(); // Uses HQL(for objects) instead of SQL(for tables)
 	}
 
 	@Override
-	@Transactional
 	public Tour getById(int id) {
 		Session session = sessionFactory.getCurrentSession();
 		return session.get(Tour.class, id);
 	}
 
 	@Override
-	@Transactional
 	public void saveOrUpdate(Tour tour) {
 		Session session = sessionFactory.getCurrentSession();
 		session.saveOrUpdate(tour);
@@ -40,7 +36,6 @@ public class TourDAOImpl implements TourDAO{
 	}
 
 	@Override
-	@Transactional
 	public void delete(int id) {
 		Session session = sessionFactory.getCurrentSession();
 		Tour tour = getById(id);
