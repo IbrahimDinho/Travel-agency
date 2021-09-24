9a8dff506db307db7f070be194d65965702f5fda
ibrahim <ibrahim@ibrahims-MacBook-Pro-2.local>
2021-09-24 17:08:14 +0100

    service layer for comment added

---
+++ src/main/service/CommentServiceImpl.java
@@ -0,0 +1,25 @@
+package main.service;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import main.dao.CommentDAO;
+import main.model.Comment;
+
+@Service
+@Transactional 
+public class CommentServiceImpl implements CommentService{
+
+	@Autowired
+	private CommentDAO commentDAO;
+	@Override
+	public void save(Comment c) {
+		// TODO Auto-generated method stub
+		commentDAO.save(c);
+		return;
+	}
+	
+	
+}
